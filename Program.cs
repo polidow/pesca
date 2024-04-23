348acfd12775398f20701b581cdb8eb4e50b5843
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-23 17:13:07 -0300

    Pesca

--- Program.cs
+++ Program.cs
@@ -6,35 +6,43 @@ int Kg;
 Console.WriteLine("------------Pesca Amadora------------");
 
 Console.WriteLine("Você esta em águas continentais e estuarinas: 1/sim 2/não");
+mar= Convert.ToInt32(Console.ReadLine());
 
-Console.ReadLine();
+        if(mar == 1)
+        {
+            Console.WriteLine("Você esta em águas continentais e estuarinas. Insira o Kg do peixe:");
+            Kg = Convert.ToInt32(Console.ReadLine());
 
-if(mar = 1);{
-Console.WriteLine("Você esta em águas continentais e estuarinas. Insira o Kg do peixe:");
-Kg = Convert.ToInt32(Console.ReadLine());
+        if (Kg > 10 )
+        {
+            Console.ForegroundColor = ConsoleColor . Red;
+            Console.WriteLine("Pescaria excede os limites legais em águas continentais e estuarinas de 10kg. Sujeito a multa de R$ 1000,00.");
+            int multa = 1000 + (20 * )
+            Console.WriteLine();
+        }
 
-if (Kg > 10);{
-Console.ForegroundColor = ConsoleColor . Red;
-Console.WriteLine("Pescaria excede os limites legais em águas continentais e estuarinas de 10kg. Sujeito a multa de R$ 1000,00.");
-}
+        else 
+        { 
+            Console.ForegroundColor = ConsoleColor . Blue;
+            Console.WriteLine("Pescaria dentro dos limites legais");
+        }
+        }
+   
+        else if (mar == 2)
+        {
+            Console.WriteLine("Você esta em águas marinhas. Insira o Kg do peixe:");
+            Kg = Convert.ToInt32(Console.ReadLine());
 
-else { 
-Console.ForegroundColor = ConsoleColor . Blue;
-Console.WriteLine("Pescaria dentro dos limites legais");
-    }
-}
+        if (Kg > 15)
+        {
+            Console.ForegroundColor = ConsoleColor . Red;
+            Console.WriteLine("Pescaria excede os limites legais em águas continentais e estuarinas de 10kg. Sujeito a multa de R$ 1000,00.");
+        }
 
-else if (mar = 2);{
-Console.WriteLine("Você esta em águas marinhas. Insira o Kg do peixe:");
-Kg = Convert.ToInt32(Console.ReadLine());
-
-if (Kg > 15);{
-Console.ForegroundColor = ConsoleColor . Red;
-Console.WriteLine("Pescaria excede os limites legais em águas continentais e estuarinas de 10kg. Sujeito a multa de R$ 1000,00.");
-}
-
-else { 
-Console.ForegroundColor = ConsoleColor . Blue;
-Console.WriteLine("Pescaria dentro dos limites legais");
-}
-}
+        else 
+        { 
+            Console.ForegroundColor = ConsoleColor . Blue;
+            Console.WriteLine("Pescaria dentro dos limites legais");
+        }
+        }
+    
